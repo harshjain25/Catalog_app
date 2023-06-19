3379f49b894095c88eaba72cad5cf9efa2f66247
Harsh Jain <jainharsh2526@gmail.com>
2023-06-20 03:30:38 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/home_page.dart';
+import 'package:flutter_catalog/pages/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,7 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      home: Material(
+        child: Center(
+            child: Container(
+          child: Text("Welcome to 30 days of flutter"),
+        )),
+      ),
     );
   }
 }
