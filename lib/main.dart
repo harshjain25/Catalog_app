55ce8aef938330e7440265873e60f9a581d36fe3
Harsh Jain <jainharsh2526@gmail.com>
2023-06-20 13:15:40 +0530

    day 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/pages/home_page.dart';
+import 'package:flutter_catalog/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-            child: Container(
-          child: Text("Welcome to 30 days of flutter"),
-        )),
-      ),
-    );
+        themeMode: ThemeMode.light,
+        theme: ThemeData(primarySwatch: Colors.deepPurple),
+        darkTheme: ThemeData(
+          brightness: Brightness.dark,
+        ),
+        routes: {
+          "/": (context) => LoginPage(),
+          // "/login":(context)=>LoginPage()
+        });
   }
 }
