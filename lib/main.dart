4a258f93682a271a0565ed6e24645bb5f82d1372
Harsh Jain <jainharsh2526@gmail.com>
2023-06-20 16:48:37 +0530

    day 4

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_catalog/pages/home_page.dart';
 import 'package:flutter_catalog/pages/login_page.dart';
 
+
 void main() {
   runApp(MyApp());
 }
@@ -11,13 +12,18 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         themeMode: ThemeMode.light,
-        theme: ThemeData(primarySwatch: Colors.deepPurple),
+        theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
+          
+          // primaryTextTheme: GoogleFonts.latoTextTheme()
+          ),
         darkTheme: ThemeData(
           brightness: Brightness.dark,
         ),
         routes: {
           "/": (context) => LoginPage(),
-          // "/login":(context)=>LoginPage()
+          "/home":(context)=>HomePage(),
+          "/login":(context)=>LoginPage()
         });
   }
 }
